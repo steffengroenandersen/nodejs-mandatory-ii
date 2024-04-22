6e853d5af80c1b540b5825e2d61151c67bf3c573
Steffen Grøn Andersen <steffengroenandersen@gmail.com>
2024-04-22 21:58:09 +0200

    Add client pages

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,1 +1,21 @@
-<h1>Hello</h1>
+<script>
+  // @ts-ignore
+  import { Router, Link, Route } from "svelte-navigator";
+  import Home from "./pages/Home/Home.svelte";
+  import Signup from "./pages/Signup/Signup.svelte";
+  import Login from "./pages/Login/Login.svelte";
+</script>
+
+<Router>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="signup">Signup</Link>
+    <Link to="/login">Login</Link>
+  </nav>
+
+  <div>
+    <Route path="/"><Home /></Route>
+    <Route path="/signup"><Signup /></Route>
+    <Route path="/login"><Login /></Route>
+  </div>
+</Router>
